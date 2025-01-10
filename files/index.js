e25cc0b955cf3623be1edae1e33bf48b656c9fdb
Jewlun <julian.boehnertkrueger@gmail.com>
2025-01-10 08:39:16 -0500

    created getEmotionsArray

--- files/index.js
+++ files/index.js
@@ -17,3 +17,25 @@ function highlightCheckedOption(e){
 
 }
 
+
+function getMatchingCatsArray(){
+    const isGif = gifsOnlyOption.checked
+    console.log(isGif)
+
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        console.log(selectedEmotion)  
+    }  
+}
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
